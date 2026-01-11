702ac0cd6bc556cab4965ba3cc4a5d38f417806c
kamranbekirovyz <kamranbekirovyz@gmail.com>
2026-01-12 01:19:24 +0400

    Add semantics to Button for screen reader support

--- templates/button.dart
+++ templates/button.dart
@@ -83,60 +83,71 @@ class _ButtonState extends State<Button> {
     final iconSize = widget._isSmall ? 18.0 : 20.0;
     final iconSpacing = widget._isSmall ? 6.0 : 8.0;
 
-    return MouseRegion(
-      onEnter: (_) {
-        if (!isDisabled) setState(() => _isHovered = true);
-      },
-      onExit: (_) {
-        if (!isDisabled) setState(() => _isHovered = false);
-      },
-      cursor: isDisabled ? SystemMouseCursors.basic : SystemMouseCursors.click,
-      child: GestureDetector(
-        onTap: isDisabled ? null : widget.onPressed,
-        child: Container(
-          padding: padding,
-          height: height,
-          width: widget._isSmall ? null : double.maxFinite,
-          decoration: BoxDecoration(
-            color: _getBackgroundColor(bg),
-            border: widget.variant == ButtonVariant.outline
-                ? Border.all(color: border, width: 1)
-                : null,
-            borderRadius: widget.variant == ButtonVariant.link
-                ? null
-                : BorderRadius.circular(12),
-          ),
-          child: Row(
-            mainAxisSize: widget._isSmall ? MainAxisSize.min : MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (widget.icon != null || widget.loading) ...[
-                if (widget.loading)
-                  SizedBox(
-                    height: iconSize,
-                    width: iconSize,
-                    child: Center(child: _LoadingSpinner(color: fg)),
-                  )
-                else if (widget.icon != null)
-                  IconTheme(
-                    data: IconThemeData(color: fg, size: iconSize),
-                    child: widget.icon!,
+    return Semantics(
+      button: true,
+      enabled: !isDisabled,
+      label: widget.label,
+      excludeSemantics: true,
+      child: MouseRegion(
+        onEnter: (_) {
+          if (!isDisabled) setState(() => _isHovered = true);
+        },
+        onExit: (_) {
+          if (!isDisabled) setState(() => _isHovered = false);
+        },
+        cursor: isDisabled
+            ? SystemMouseCursors.basic
+            : SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: isDisabled ? null : widget.onPressed,
+          child: Container(
+            padding: padding,
+            height: height,
+            width: widget._isSmall ? null : double.maxFinite,
+            decoration: BoxDecoration(
+              color: _getBackgroundColor(bg),
+              border: widget.variant == ButtonVariant.outline
+                  ? Border.all(color: border, width: 1)
+                  : null,
+              borderRadius: widget.variant == ButtonVariant.link
+                  ? null
+                  : BorderRadius.circular(12),
+            ),
+            child: Row(
+              mainAxisSize: widget._isSmall
+                  ? MainAxisSize.min
+                  : MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (widget.icon != null || widget.loading) ...[
+                  if (widget.loading)
+                    SizedBox(
+                      height: iconSize,
+                      width: iconSize,
+                      child: Center(child: _LoadingSpinner(color: fg)),
+                    )
+                  else if (widget.icon != null)
+                    IconTheme(
+                      data: IconThemeData(color: fg, size: iconSize),
+                      child: widget.icon!,
+                    ),
+                  SizedBox(width: iconSpacing),
+                ],
+                Text(
+                  widget.label,
+                  style: TextStyle(
+                    color: fg,
+                    fontSize: fontSize,
+                    decorationColor: fg,
+                    fontWeight: FontWeight.w500,
+                    decoration:
+                        widget.variant == ButtonVariant.link && _isHovered
+                        ? TextDecoration.underline
+                        : null,
                   ),
-                SizedBox(width: iconSpacing),
-              ],
-              Text(
-                widget.label,
-                style: TextStyle(
-                  color: fg,
-                  fontSize: fontSize,
-                  decorationColor: fg,
-                  fontWeight: FontWeight.w500,
-                  decoration: widget.variant == ButtonVariant.link && _isHovered
-                      ? TextDecoration.underline
-                      : null,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),

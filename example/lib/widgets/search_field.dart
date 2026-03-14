4bb05d705b5ca4ed138e9dfd5eb980a9733b15dc
kamranbekirovyz <kamranbekirovyz@gmail.com>
2026-03-14 19:44:11 +0400

    style: simplify SearchField style comments

--- example/lib/widgets/search_field.dart
+++ example/lib/widgets/search_field.dart
@@ -4,12 +4,12 @@ import 'package:flutter/widgets.dart';
 
 // Customizable constants
 //
-// Default: Compact Cupertino-like style (iOS only, not recommended for Android)
+// Default: Compact
 const double _kHeight = 36;
 const double _kHorizontalPadding = 8;
 const double _kBorderRadius = 10;
 //
-// Alternative: Universal pill style (works on all platforms)
+// Alternative: Pill style
 // const double _kHeight = 44;
 // const double _kHorizontalPadding = 14;
 // const double _kBorderRadius = 22;
